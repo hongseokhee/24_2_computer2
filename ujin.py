ed41520ab7e7c9fde008536966abc6e49239996c
Ujin-Heo <63441458+Ujin-Heo@users.noreply.github.com>
2024-12-01 11:14:49 +0900

    draw_graph 함수 한글 깨짐 해결

--- ujin.py
+++ ujin.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
+# 터미널에서 폰트 미리 설치해야 됨
+# sudo apt-get install -y fonts-nanum
+# sudo fc-cache -fv
+# rm ~/.cache/matplotlib -rf
+from matplotlib import rcParams
+plt.rc('font', family='NanumGothic')
+rcParams['axes.unicode_minus'] = False
+
 def draw_graph(data, domain, category):
     data_for_graph = data[domain][category]
     standard = data_for_graph[0]
@@ -10,18 +18,18 @@ def draw_graph(data, domain, category):
 
     plt.figure(figsize=(8, 6))
 
-    plt.plot(standard, male, label='male', color='blue')
-    plt.plot(standard, female, label='female', color='orange')
+    plt.plot(standard, male, label='남', color='blue')
+    plt.plot(standard, female, label='여', color='orange')
 
-    plt.title(f"2024 Suneung {category} Subject Distribution", fontsize=14)
-    plt.xlabel("Standard Socre", fontsize=12)
-    plt.ylabel("Number of Students", fontsize=12)
+    plt.title(f"2024 수능 {category} 과목 분포", fontsize=14)
+    plt.xlabel("표준점수", fontsize=12)
+    plt.ylabel("명수", fontsize=12)
     plt.legend(fontsize=10)
 
     plt.grid(alpha=0.3)
 
     plt.savefig(f"{domain}_{category}_graph.png", dpi=300)
-    print(f"Graph saved as {domain}_{category}_graph.png")
+    print(f"{domain}_{category}_graph.png라는 파일명으로 그래프 저장됨")
 
     plt.close()
 

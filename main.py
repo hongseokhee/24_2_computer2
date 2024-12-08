f44eb0ca13b5907040d3a7f0817599887574b0c1
nuewsun <sunnyy0905@gmail.com>
2024-12-08 22:12:18 +0900

    while문 반복 처리

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 import eunsun, ujin
 
 def isvalid(realm):
+    return realm in df
+
+def subject(realm):
+    return list(set(df[realm]))
+
+import eunsun, ujin
+
+def isvalid(realm):
     realmfalse = 0
     if realm not in df:
         realmfalse = 1
@@ -25,19 +33,33 @@ while True:
         print(f"데이터 상에 {wanna_know_year}년은 없습니다.")
         continue
     df = eunsun.save_data(f'{wanna_know_year}1231.csv')
-    wanna_know_realm = input("영역을 입력하세요(데이터 상 존재하는 영역: 국어, 수학, 사회탐구, 과학탐구, 직업탐구) : ")
-    valid_data = isvalid(wanna_know_realm)
-    if valid_data != 1:
-        temp = ", ".join(subject(wanna_know_realm))
-        wanna_know_subject = input(f"과목을 입력하세요. 과목은 다음과 같습니다. ({temp}) : ")
-        if wanna_know_subject not in df[wanna_know_realm]:
-            print(f"{wanna_know_subject} 과목은 존재하지 않습니다.")
-        else:
-            data_type = int(input("그래프를 그리는 데 사용할 데이터의 종류를 선택해주세요. 1) 명수 2) 비율 (1 또는 2 입력): "))
-            if data_type == 1 or data_type == 2:
-                ujin.draw_graph(df, wanna_know_realm, wanna_know_subject, data_type)
-                break
-            else:
-                print(f"{data_type}라는 데이터 종류는 없습니다.")
-    else:
-        print(f"데이터에 {wanna_know_realm} 영역은 없습니다.")
+    
+    while True:  # 영역 
+        wanna_know_realm = input("영역을 입력하세요(데이터 상 존재하는 영역: 국어, 수학, 사회탐구, 과학탐구, 직업탐구) : ")
+        valid_data = isvalid(wanna_know_realm)
+        if valid_data == 1:
+            print(f"데이터에 {wanna_know_realm} 영역은 없습니다.")
+            continue
+        
+        while True:  # 과목 
+            temp = ", ".join(subject(wanna_know_realm))
+            wanna_know_subject = input(f"과목을 입력하세요. 과목은 다음과 같습니다. ({temp}) : ")
+            if wanna_know_subject not in df[wanna_know_realm]:
+                print(f"{wanna_know_subject} 과목은 존재하지 않습니다.")
+                continue
+            
+            while True:  # 데이터 종류
+                try:
+                    data_type = int(input("그래프를 그리는 데 사용할 데이터의 종류를 선택해주세요. 1) 명수 2) 비율 (1 또는 2 입력): "))
+                    if data_type not in [1, 2]:
+                        print(f"{data_type}라는 데이터 종류는 없습니다.")
+                        continue
+                    ujin.draw_graph(df, wanna_know_realm, wanna_know_subject, data_type)
+                    break  
+                except ValueError:
+                    print("잘못된 입력입니다. 1 또는 2를 입력해주세요.")
+                    continue
+            break  
+        break  
+    break  
+
